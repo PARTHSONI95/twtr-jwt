b055c987a312c282ff7c63cd4cb56c6311fcf28e
Parth Soni <parthsangita95@yahoo.in>
2021-04-03 17:29:16 -0400

    Twitter Analysis

---
+++ fe/build/precache-manifest.2f93ca7130f4aaad6a0626cbff332965.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78114074358baaa4de3a793b4a99716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef664102893085ee0d39",
+    "url": "/static/css/main.643b110a.chunk.css"
+  },
+  {
+    "revision": "51a18b08900f4a85cfe0",
+    "url": "/static/js/2.54b9e76a.chunk.js"
+  },
+  {
+    "revision": "2f90c8634b386e36b9c7e8327c2fcf69",
+    "url": "/static/js/2.54b9e76a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef664102893085ee0d39",
+    "url": "/static/js/main.a9e80681.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
